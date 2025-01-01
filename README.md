b66fcf6f057c6fa881efc2ada6c4270bab84170c
Karl Ender <karlender24@gmail.com>
2025-01-01 17:08:17 +0100

    Basic Spring Boot setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# CSGR - Case Study Groupchat Roulette
+
+## Neccessary IntelliJ Plugins
+
+- mermaid - Documentation charts
+
+## Backend
+
+### Setup
+
+// TODO
+
+## Frontend
+
+### Setup
+
+// TODO
---
+++ csgr-backend/src/main/java/de/csgr/CsgrApplication.java
@@ -0,0 +1,12 @@
+package de.csgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsgrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsgrApplication.class, args);
+    }
+}
